86617538f2dcab363b5b58b1cc4a058a2fcb1230
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-03-26 12:43:41 +0300

    Implemented database connection eith model and migrations. #11

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -14,6 +15,15 @@ class MessagesController extends Controller
             'message' => 'required|min:10'
         ]);
 
-        return 'SUCCESS';
+        //  Create new message and save it.
+        $message = new Message();
+
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+        $message->save();
+
+        //  Redirect
+        return redirect('/')->with('success', 'Message sent');
     }
 }
